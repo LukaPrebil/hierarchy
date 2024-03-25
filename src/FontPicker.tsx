eb940e26aac0d30462f90886807375c79d41678d
Luka Prebil Grintal <pgluka7@gmail.com>
2024-03-25 12:51:01 +0100

    Material top bar

--- src/FontPicker.tsx
+++ src/FontPicker.tsx
@@ -1,18 +1,31 @@
+import { InputLabel, MenuItem, Select } from "@mui/material";
 import React from "react";
 
-export const FontPicker = () => {
+type FontPickerProps = {
+    font: string;
+    setFont: (font: string) => void;
+}
+
+export const FontPicker: React.FC<FontPickerProps> = ({font, setFont}) => {
   const fonts = React.useMemo(() => {
     const fonts = document.fonts.values();
     return new Set([...fonts].map((font) => font.family));
   }, []);
 
   return (
-    <select>
+    <>
+      <InputLabel id="font-select-label">Font</InputLabel>
+      <Select
+        labelId="font-select-label"
+        id="font-select"
+        value={font}
+        label="Font"
+        onChange={(e) => setFont(e.target.value)}
+      >
         {[...fonts].map((font) => (
-            <option key={font} value={font}>
-            {font}
-            </option>
+            <MenuItem key={font} value={font}>{font}</MenuItem>
         ))}
-    </select>
-  )
+      </Select>
+    </>
+  );
 };
--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./TopBar.css";
 import { FontPicker } from "./FontPicker";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import Toolbar from "@mui/material/Toolbar";
+import FormControl from "@mui/material/FormControl";
 
 export const TopBar: React.FC = () => {
+  const [font, setFont] = React.useState("Roboto");
   return (
-    <div className="top-bar">
-      <h2>Hierarchy</h2>
-      <FontPicker />
-    </div>
+    <Box sx={{ flexGrow: 1, width: "100%", marginBottom: "1rem" }}>
+      <AppBar position="static">
+        <Toolbar  sx={{height: "5rem", alignItems: "center"}}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Hierarchy
+          </Typography>
+          <FormControl>
+            <FontPicker font={font} setFont={setFont} />
+          </FormControl>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
